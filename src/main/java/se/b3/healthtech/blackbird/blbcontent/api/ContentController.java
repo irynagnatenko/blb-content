7dbfab253f742ec456def4b5ef0468d94506f8d6
irynagnatenko <Ulibka1023>
2022-07-08 12:19:29 +0200

    TB-63 - added deleteContent method in the controller, service and DBhandler classes, added getLatestContent method in the controller, service and DBhandler classes, created ServiceUtil class

--- src/main/java/se/b3/healthtech/blackbird/blbcontent/api/ContentController.java
+++ src/main/java/se/b3/healthtech/blackbird/blbcontent/api/ContentController.java
@@ -24,7 +24,8 @@ public class ContentController {
     public ContentController(ContentService contentService) {
         this.contentService = contentService;
     }
-    @Operation(summary = "Create a new list of text blocks")
+
+    @Operation(summary = "Create a list of contents")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully created a list of text blocks", content = {@Content}),
             @ApiResponse(responseCode = "404", description = "Object not found", content = {@Content}),
@@ -35,7 +36,7 @@ public class ContentController {
         contentService.addContentList(key, contentList);
     }
 
-    @Operation(summary = "Get the latest content")
+    @Operation(summary = "Get the list of the latest content")
     @ApiResponses(value = {
             // Om man har en List, lägger man till @Schema
             @ApiResponse(responseCode = "200", description = "Found content",
@@ -52,18 +53,18 @@ public class ContentController {
         return contentService.getLatestContent(key);
     }
 
-    @Operation(summary = "Create a new content block")
+    @Operation(summary = "Create a new content object")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Successfully created a text block", content = {@Content}),
             @ApiResponse(responseCode = "404", description = "Object not found", content = {@Content}),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content})})
-    @PostMapping(value = "/")
+    @PostMapping(value = "/",
+        params = "key")
     @ResponseStatus(value = HttpStatus.OK)
-    public void addContent(@RequestParam String key, @RequestBody se.b3.healthtech.blackbird.blbcontent.model.Content content) throws CloneNotSupportedException {
+    public void addContent(@RequestParam("key") String key, @RequestBody se.b3.healthtech.blackbird.blbcontent.model.Content content) throws CloneNotSupportedException {
         contentService.addContent(key, content);
     }
 
-    /*
     @Operation(summary = "Get the latest content for a specific partitionKey")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "200", description = "Found content object",
@@ -72,13 +73,28 @@ public class ContentController {
             @ApiResponse(responseCode = "404", description = "Object not found", content = {@Content}),
             @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content})})
     @GetMapping(value = "/",
-            params = {"key", "ContentId"},
+            params = {"key", "id"},
             produces = {"application/json"})
     @ResponseStatus(value = HttpStatus.OK)
-    public Content getContent(@RequestParam("key") String key, @RequestParam("contentId") String contentId) {
+    public se.b3.healthtech.blackbird.blbcontent.model.Content getContent(@RequestParam("key") String publicationId, @RequestParam("id") String contentId) {
         log.info("in ContentController - getContent");
-        return contentService.getcontent(key, contentId);
+        return contentService.getContent(publicationId, contentId);
     }
 
-     */
+    @Operation(summary = "Delete content object")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully deleted a text block", content = {@Content}),
+            @ApiResponse(responseCode = "404", description = "Object not found", content = {@Content}),
+            @ApiResponse(responseCode = "500", description = "Internal Server Error", content = {@Content})})
+    @PostMapping(value = "/",
+            headers = "userName",
+            params = "key",
+            produces = {"application/json"})
+    @ResponseStatus(value = HttpStatus.OK)
+    public void deleteContent(@RequestHeader("userName") String userName,
+                              @RequestParam("key") String publicationId,
+                              @RequestBody List<se.b3.healthtech.blackbird.blbcontent.model.Content> contentList) {
+        log.info("in ContentController - deleteContent");
+        contentService.deleteContent(userName, publicationId, contentList);
+    }
 }
